c5a41ff1bacf72774b9b2e0f98f43eca7ba48a46
Justin Clark <jclark@cyber.law.harvard.edu>
2012-07-23 16:40:41 -0400

    clean up everything

--- add_geo.rb
+++ add_geo.rb
@@ -1,7 +1,5 @@
 require 'json/ext'
 require 'nokogiri'
-require 'liquid'
-require 'pp'
 require 'erb'
 
 class GeoTags < Nokogiri::XML::SAX::Document
@@ -49,19 +47,11 @@ class GeoTags < Nokogiri::XML::SAX::Document
     end
 end
 
-module MyFilters
-    def clean(input)
-        input.gsub(/\A[^\[\w\(]+|[^\w\)\]]+\z/, '') if input
-    end
-end
-
 class String
     def clean
         self.gsub(/\A[^\[\w\(]+|[^\w\)\]]+\z/, '')
     end
 end
-
-Liquid::Template.register_filter(MyFilters)
 
 contents = File.read 'sample_files/dpla_records.json'
 end_chars = []
@@ -80,29 +70,9 @@ $json_contents = JSON.parse(contents)
 
 parser = Nokogiri::XML::SAX::Parser.new(GeoTags.new(end_chars))
 parser.parse(File.open('sample_files/geotags.xml'))
-template = Liquid::Template.parse(File.read('solr_record_template.xml.liquid'))
+
 erb_template = ERB.new(File.read('solr_record_template.xml.erb'))
 
 docs = $json_contents['docs']
-records = erb_template.result(binding)
-# $json_contents['docs'].each do |doc|
-#     doc['geotags'].each do |geotag|
-# #        rendered = template.render(
-# #            'geotag' => geotag,
-# #            'doc' => doc,
-# #            'json' => doc.to_json,
-# #            'marc_fields' => [
-# #                '100a', '100c', '100d', '245a', '245c', '260a', '260b', '260c',
-# #                '300a', '300c', '500a', '655a', '700a', '700d', '700e', '752a',
-# #                '752d', '440a', '440p', '830a', '830p', '840a', '130a', '730a',
-# #                '240a', '260f', '260e', '752b', '034d', '034e', '034f', '034g',
-# #                '245b', '245c', '245p', '246a', '740a', '600y', '610y', '611y',
-# #                '630y', '650y', '651y', '655y', '690y', '691y', '692y', '693y',
-# #                '694y', '695y', '600v', '610v', '611v', '630v', '650v', '651v',
-# #                '655b', '655v', '690v', '691v', '692v', '693v', '694v', '695v'
-# #            ]
-# #        )
-#         records += erb_template.result(binding)
-#     end
-# end
-File.open("output/test2.xml", 'w') { |f| f.write(records) }
+records = erb_template.result(binding)#.gsub(/^\s*\n/,'')
+File.open("output/test.xml", 'w') { |f| f.write(records) }
